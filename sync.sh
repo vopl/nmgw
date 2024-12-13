778edbf0f9ac37a88641e5db10a410d154443ac6
vopl <vopl@bk.ru>
2024-12-13 17:39:55 +0300

    .

--- sync.sh
+++ sync.sh
@@ -3,18 +3,19 @@ set -e
 host=185.185.40.120
 
 if [[ "$#" -ne 1 ]]; then
-  echo "usage: $0 outApk|outElf" >&2
+  echo "usage: $0 outAndroid|outLinux" >&2
   exit 1
 fi
 
-do_outApk=no
+do_outAndroid=no
+do_outLinux=no
 
 case $1 in
-    "outApk")
-        do_outApk=yes
+    "outAndroid")
+        do_outAndroid=yes
     ;;
-    "outElf")
-        do_outElf=yes
+    "outLinux")
+        do_outLinux=yes
     ;;
     *)
         echo "unrecognized arg: $1"
@@ -25,14 +26,26 @@ esac
 
 opts="-avr -ztu -h --info=progress2 -e 'ssh -p 36271'"
 
-if [[ "$do_outApk" == "yes" ]]; then
-    cmd="rsync $opts ./bin/{entry,gate}.apk nmgw@${host}:/home/nmgw/binaries/$(date +%4Y-%m-%d)/"
+if [[ "$do_outAndroid" == "yes" ]]; then
+    cmd="rsync $opts ./bin/{entry,gate}.apk nmgw@${host}:/home/nmgw/binaries/$(date +%4Y-%m-%d)/bin/"
     echo sync local to remote: $cmd
     sh -c "$cmd"
 fi
 
-if [[ "$do_outElf" == "yes" ]]; then
-    cmd="rsync $opts ./bin/{entry,gate,rendezvous} nmgw@${host}:/home/nmgw/binaries/$(date +%4Y-%m-%d)/"
+if [[ "$do_outLinux" == "yes" ]]; then
+    cmd="ssh -p 36271 nmgw@${host} mkdir -p /home/nmgw/binaries/$(date +%4Y-%m-%d)"
+    echo sync local to remote: $cmd
+    sh -c "$cmd"
+
+    cmd="rsync $opts ./bin/ nmgw@${host}:/home/nmgw/binaries/$(date +%4Y-%m-%d)/bin/"
+    echo sync local to remote: $cmd
+    sh -c "$cmd"
+
+    cmd="rsync $opts ./lib/ nmgw@${host}:/home/nmgw/binaries/$(date +%4Y-%m-%d)/lib/"
+    echo sync local to remote: $cmd
+    sh -c "$cmd"
+
+    cmd="rsync $opts ./etc/ nmgw@${host}:/home/nmgw/binaries/$(date +%4Y-%m-%d)/etc/"
     echo sync local to remote: $cmd
     sh -c "$cmd"
 fi
